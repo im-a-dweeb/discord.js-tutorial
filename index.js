fa913fd6e58c435982a9ae637c732c4b0b61d27f
im-a-dweeb <127716957+im-a-dweeb@users.noreply.github.com>
2023-04-23 22:22:02 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, Events, GatewayIntentBits, SlashCommandBuilder} = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]});
+
+client.once(Events.ClientReady, async c => {
+    console.log(`Client logged in as ${c.user.tag}`)
+    const command = new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Ping pong command');
+
+    
+    const commandData = command.toJSON();
+    await client.application.commands.create(commandData)
+})
+
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    if(interaction.commandName === 'ping') {
+        await interaction.reply('Pong')
+    }
+})
+
+
+client.login("MTA5OTg5NDg4NTQ2MjU4OTQ1MA.GKhlJl.gG2532MxvrfrGQ3A65vIqjkVRbOmTeSw6JuInM");
